f2a9d5d2d96d54d87b1fd782c63a6c148f625075
yanyadi <yadihaoku@126.com>
2017-04-17 01:00:56 +0800

    添加清大100地址

---
+++ javatest/src/main/java/com/example/spider/Qing100cn.java
@@ -0,0 +1,14 @@
+package com.example.spider;
+
+/**
+ * Created by yanyadi on 2017/4/17.
+ */
+
+public class Qing100cn {
+
+    /**
+     * 清华大学出版社
+     * 大图，在源码中 <li><a></a><li/> a 标签中。
+     */
+    public static final String NINE_CLASS_URL = "http://www.qingda100.cn/show/2132.html";
+}
